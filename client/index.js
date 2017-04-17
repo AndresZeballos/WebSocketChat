e44faf502f83cfc972797a0d41b86ccef94e129a
AndresZeballos <andreszeballosjuanico@gmail.com>
2017-04-17 17:59:19 -0300

    Inital

---
+++ client/index.js
@@ -0,0 +1,32 @@
+var socket = io.connect('smarthome-az.appspot.com:8080', {'forceNew':true})
+
+socket.on('messages', function(data){
+	console.log(data);
+	renderMessages(data);
+});
+
+function renderMessages(messages){
+	var html = messages.map(function(message, index){
+
+		return (`
+			<div class="message">
+				<strong>${message.user}</strong> dice: 
+				<p>${message.text}</p>
+			</div>
+			`);
+	}).join(' ');
+
+	document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e){
+	var message = {
+		user: document.getElementById('user').value,
+		text: document.getElementById('text').value
+	}
+	document.getElementById('user').style.display = 'none';
+	socket.emit('add-message', message);
+	return false;
+}
+
+
---
+++ server/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('client'));
+
+app.get('/', function(req, res) {
+  res.status(200).send('Hello motherfuckers');
+});
+
+var messages = [{
+	id: 1,
+	text: 'Bienvenido!',
+	user: 'Owner'
+}]
+
+io.on('connection', function(socket){
+
+	console.log('Nuevo cliente conectado ' + socket.handshake.address);
+	socket.emit('messages', messages);
+
+	socket.on('add-message', function(data){
+		messages.push(data);
+		io.sockets.emit('messages', messages);
+	})
+});
+
+
+
+server.listen(8080, function() {
+	console.log('Server runing on http://localhost:8080');
+});
+
+
+
